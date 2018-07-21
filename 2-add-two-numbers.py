7953798e0ccf280f8bd0a0acce59512c9556ff05
Njenga Saruni <njengasaruni@gmail.com>
2018-07-21 20:34:01 +0300

    2 - Add two numbers, listnode representation

---
+++ 2-add-two-numbers.py
@@ -0,0 +1,52 @@
+import unittest
+
+
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+    def extend(self, x):
+        current = self
+        previous = None
+        while current is not None:
+            previous = current
+            current = current.next
+
+        previous.next = ListNode(x)
+
+    def __repr__(self):
+        repr = ''
+        current = self
+        while current is not None:
+            if current.next is not None:
+                repr += '{} -> '.format(current.val)
+                current = current.next
+            else:
+                return repr + str(current.val)
+
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode"""
+
+        pass
+
+
+# class TestSolution(unittest.TestCase):
+#     solution = Solution()
+#
+#     def test_simple(self):
+#         pass
+
+
+if __name__ == '__main__':
+    # unittest.main()
+    ln = ListNode(2)
+    ln.extend(3)
+    ln.extend(5)
+
+    print(ln)
