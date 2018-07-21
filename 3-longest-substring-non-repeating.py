5a5fd1f321ee5ca34adcbd5e2cbf89c1345a3c2c
Njenga Saruni <njengasaruni@gmail.com>
2018-07-22 00:22:28 +0300

    Working solution 3 with tests

--- 3-longest-substring-non-repeating.py
+++ 3-longest-substring-non-repeating.py
@@ -8,13 +8,31 @@ class Solution(object):
         :rtype: int
         """
 
+        longest = ''
+        current = ''
+        for i in s:
+            if i not in current:
+                current += i
+                continue
+
+            if len(current) > len(longest):
+                longest = current
+
+            current = i
+
+        return longest
 
 class TestSolution(unittest.TestCase):
     solution = Solution()
 
     def test_simple(self):
-        pass
+        self.assertEqual(self.solution.lengthOfLongestSubstring('abcabcbb'), 'abc')
 
+    def test_single_char(self):
+        self.assertEqual(self.solution.lengthOfLongestSubstring('bbbbbbbb'), 'b')
+
+    def test_longest_in_middle(self):
+        self.assertEqual(self.solution.lengthOfLongestSubstring('pwwkew'), 'wke')
 
 if __name__ == '__main__':
     unittest.main()
