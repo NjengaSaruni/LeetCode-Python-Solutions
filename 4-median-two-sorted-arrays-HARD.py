84afd231c0713321fca83a6fc901a8d244411198
Njenga Saruni <njengasaruni@gmail.com>
2018-07-22 21:11:30 +0300

    - Add problem descriptions in files - Rename files to include difficulty

---
+++ 4-median-two-sorted-arrays-HARD.py
@@ -0,0 +1,36 @@
+import unittest
+
+# There are two sorted arrays nums1 and nums2 of size m and n respectively.
+#
+# Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+#
+# Example 1:
+# nums1 = [1, 3]
+# nums2 = [2]
+#
+# The median is 2.0
+# Example 2:
+# nums1 = [1, 2]
+# nums2 = [3, 4]
+#
+# The median is (2 + 3)/2 = 2.5
+
+class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+
+        return 1
+
+class TestSolution(unittest.TestCase):
+    solution = Solution()
+
+    def test_simple(self):
+        self.assertEqual(self.solution.findMedianSortedArrays([1], [1]), 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
