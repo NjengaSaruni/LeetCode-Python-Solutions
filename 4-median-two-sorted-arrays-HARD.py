37748e16b6a78a7d015212b96ff136b3e419d223
Njenga Saruni <njengasaruni@gmail.com>
2018-07-23 10:17:20 +0300

    Start recursive approach to problem

--- 4-median-two-sorted-arrays-HARD.py
+++ 4-median-two-sorted-arrays-HARD.py
@@ -1,5 +1,6 @@
 import unittest
 
+
 # There are two sorted arrays nums1 and nums2 of size m and n respectively.
 #
 # Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
@@ -16,21 +17,49 @@ import unittest
 # The median is (2 + 3)/2 = 2.5
 
 class Solution(object):
+    def getLocationOfMedian(self, number, sortedArray):
+        sortedArrayLength = len(sortedArray)
+        halfSortedArrayLength = sortedArrayLength // 2
+
+        numberOfItemsLeft = halfSortedArrayLength
+        numberOfItemsRight = halfSortedArrayLength - 1
+
+        pivot = sortedArray[halfSortedArrayLength]
+
     def findMedianSortedArrays(self, nums1, nums2):
         """
         :type nums1: List[int]
         :type nums2: List[int]
         :rtype: float
         """
+        lenOfNums1 = len(nums1)
+        lenOfNums2 = len(nums2)
 
-        return 1
 
-class TestSolution(unittest.TestCase):
-    solution = Solution()
+        if lenOfNums1 % 2 == 0:
+            median1 = (nums1[lenOfNums1 // 2] + nums1[lenOfNums1 // 2 - 1]) / 2
+        else:
+            median1 = nums1[lenOfNums1 // 2]
 
-    def test_simple(self):
-        self.assertEqual(self.solution.findMedianSortedArrays([1], [1]), 1)
+        if lenOfNums2 % 2 == 0:
+            median2 = (nums2[lenOfNums2 // 2] + nums2[lenOfNums2 // 2 - 1]) / 2
+        else:
+            median2 = nums2[lenOfNums2 // 2]
+
+        self.getLocationOfMedian(median1, nums2)
+
+
+# class TestSolution(unittest.TestCase):
+#     solution = Solution()
+#
+#     def test_simple(self):
+#         self.assertEqual(self.solution.findMedianSortedArrays([1], [1]), 1)
 
 
 if __name__ == '__main__':
-    unittest.main()
+    solution = Solution()
+
+    arr1 = [1, 7, 12, 12]
+    arr2 = [2, 5, 5, 8, 9]
+
+    solution.findMedianSortedArrays(arr1, arr2)
