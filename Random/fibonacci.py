0d2c9baf4e611e4c3f4c6d625fd32091e3255ac7
Peter Njenga <njengasaruni@gmail.com>
2019-01-17 17:21:49 +0300

    Memoization for fibonacci

--- Random/fibonacci.py
+++ Random/fibonacci.py
@@ -1,4 +1,6 @@
 import unittest
+import timeit
+import functools
 
 def fibonacci(order):
     a = 1
@@ -11,18 +13,45 @@ def fibonacci(order):
 
     return b
 
-class TestSolution(unittest.TestCase):
-    def setUp(self):
-        self.function = fibonacci
 
-    def test_zero(self):
-        self.assertEqual(self.function(0), 1)
+def fibonacciRecursive(order):
+    if order <= 1:
+        return 1
+    return fibonacci(order - 1) + fibonacci(order - 2)
 
-    def test_two(self):
-        self.assertEqual(self.function(1), 1)
+def _getFibonacci(memo, order):
+    if memo.__contains__(order):
+        return memo[order]
+    value = _getFibonacci(memo, order - 1) + _getFibonacci(memo, order - 2)
+    memo[order] = value
+    return value
 
-    def test_two(self):
-        self.assertEqual(self.function(2), 2)
+def fibonacciRecursiveMemoized(order):
+    d = {
+        0: 1,
+        1: 1
+    }
+    return _getFibonacci(d, order)
 
-    def test_five(self):
-        self.assertEqual(self.function(8), 34)
+# class TestSolution(unittest.TestCase):
+#     def setUp(self):
+#         self.function = fibonacciRecursiveMemoized
+#
+#     def test_zero(self):
+#         self.assertEqual(self.function(0), 1)
+#
+#     def test_two(self):
+#         self.assertEqual(self.function(1), 1)
+#
+#     def test_five(self):
+#         self.assertEqual(self.function(8), 34)
+
+
+if __name__ == '__main__':
+    # iterative = timeit.Timer(functools.partial(fibonacci, 121))
+    recursive = timeit.Timer(functools.partial(fibonacciRecursive, 121))
+    amoized = timeit.Timer(functools.partial(fibonacciRecursiveMemoized, 121))
+
+    # print('Iterative: ', iterative.timeit())
+    print('Recursive: ', recursive.timeit())
+    print('Amoized: ', amoized.timeit())
