8e7f7db88f6d4ed17b73afbafed89991a26d7bf2
Peter Njenga <njengasaruni@gmail.com>
2019-01-17 12:18:28 +0300

    Folds in fractal problem

---
+++ Random/fractal.py
@@ -0,0 +1,17 @@
+def fractal(folds):
+    current = ''
+
+    for i in range(folds):
+        previous = current
+        current += 'L'
+        for j in previous:
+            if j == 'L':
+                current += 'R'
+            else:
+                current += 'L'
+
+    return current
+
+
+if __name__ == '__main__':
+    print(fractal(3))
