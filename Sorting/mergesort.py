703acd6fa7427e0bb568b0e58f5357a2ec3b06e8
Peter Njenga <njengasaruni@gmail.com>
2019-01-20 22:02:43 +0300

    Correctly working mergeSort

---
+++ Sorting/mergesort.py
@@ -0,0 +1,42 @@
+import unittest
+
+
+def mergeSort(ls):
+    if len(ls) <= 1:
+        return ls
+
+    ans = []
+
+    mid = len(ls) // 2
+    left = mergeSort(ls[:mid])
+    right = mergeSort(ls[mid:])
+
+    i = 0
+    j = 0
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            ans.append(left[i])
+            i += 1
+        else:
+            ans.append(right[j])
+            j += 1
+
+    while i < len(left):
+        ans.append(left[i])
+        i += 1
+
+    while j < len(right):
+        ans.append(right[j])
+        j += 1
+
+    print(ans)
+    return ans
+
+
+class TestMergeSort(unittest.TestCase):
+    def test_simple(self):
+        self.assertEqual(mergeSort([1, 5, 2, 3, 4, 8, 1]), [1, 1, 2, 3, 4, 5, 8])
+
+
+if __name__ == '__main__':
+    unittest.main()
