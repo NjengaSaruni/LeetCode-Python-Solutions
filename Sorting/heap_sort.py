6f9c02df50dc79a7fc722bc09ac0f9f1e12b8f74
Peter Njenga <njengasaruni@gmail.com>
2019-01-21 20:40:37 +0300

    Use inbuilt heapsort inbuilt for sorting

--- Sorting/heap_sort.py
+++ Sorting/heap_sort.py
@@ -1,4 +1,5 @@
 import unittest
+from heapq import heappop, heappush
 
 def build_max_hip(ls):
 
@@ -6,9 +7,18 @@ def build_max_hip(ls):
 def heap_sort(ls):
     print(ls)
 
+def heapsort_inbuilt(iterable):
+    h = []
+    for value in iterable:
+        heappush(h, value)
+    return [heappop(h) for i in range(len(h))]
+
 class TestHeapSort(unittest.TestCase):
+    def setUp(self):
+        self.fn = heapsort_inbuilt
+
     def test_simple_array(self):
-        self.assertEqual(heap_sort([1, 4, 5, 3, 2]), [1, 2, 3, 4, 5])
+        self.assertEqual(self.fn([1, 4, 5, 3, 2]), [1, 2, 3, 4, 5])
 
 if __name__ == '__main__':
     unittest.main()
