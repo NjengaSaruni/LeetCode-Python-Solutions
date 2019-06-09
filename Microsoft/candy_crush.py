43b760d5853f614ad1edd94f67a41f6f1c373f1d
Peter Njenga <peternjengask@gmail.com>
2019-06-09 11:54:04 +0300

    More utility methods for a linked list

--- Microsoft/candy_crush.py
+++ Microsoft/candy_crush.py
@@ -15,6 +15,38 @@ class ListNode(object):
         self.value = value
         self.next = None
 
+    def __eq__(self, other):
+        """
+        Compare two ListNode instance for structural equality
+        Two ListNode instance are considered equal if the values stored at their nodes
+        are equal (by value for primitives and by reference for objects)
+        :param other: The second ListNode instance to be compared
+        :return: True if self and other are structurally equal, otherwise False
+        """
+
+        # Two ListNode instance cannot be considered equal if one is longer than the other
+        if not other:
+            return False
+
+        # Performs a recursive comparison of next node if the values are the same
+        # If the values are not the same then the nodes are not structurally unequal
+        return self.value == other.value and self.next == other.next
+
+    def __str__(self):
+        """
+        Create a string representation of values to be stored from self to the end of the linked list
+        :return:
+        """
+        string_representation = ''
+        current = self
+
+        # Iteratively appends the current value of the ListNode to the string representation
+        while current is not None:
+            string_representation = f'{string_representation}{current.value} => '
+            current = current.next
+
+        return f'{string_representation}None'
+
 
 class SinglyLinkedList(object):
     """
@@ -47,17 +79,15 @@ class SinglyLinkedList(object):
 
         return self
 
-    def __str__(self):
-        current = self.root
+    def __str__(self) -> str:
+        """
+        Creates a string representation of a SinglyLinkedList
+        :return: The str representation of a SinglyLinkedList from its root onwards
+        """
+        return str(self.root)
 
-        string_representation = ''
-
-        while current is not None:
-            string_representation = f'{string_representation}{current.value} => '
-            current = current.next
-
-        return f'{string_representation}None'
-
+    def __eq__(self, other):
+        return self.root == other.root
 
 if __name__  == '__main__':
     sll = SinglyLinkedList()
