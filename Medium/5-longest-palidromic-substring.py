569a83e555912f0a2f77328019d8ebf5b0f67a60
Peter Njenga <peternjengask@gmail.com>
2019-06-15 23:46:45 +0300

    Dynamic programming using a set

--- Medium/5-longest-palidromic-substring.py
+++ Medium/5-longest-palidromic-substring.py
@@ -22,64 +22,37 @@ class Solution(object):
         :rtype: str
         """
 
-        mid = len(s) // 2
-        left_even = mid - 1
-        right_even = mid
-        best = (mid, mid)
+        longest = ''
 
-        left_odd, right_odd = self.longestPalindromeMiddleOutOdd(s, mid)
-        if left_even >= 0 and s[left_even] == s[right_even]:
-            left_even, right_even = self.longestPalindromeMiddleOutEven(s, mid)
+        palindromes = set()
 
-        if right_even - left_even > right_odd - left_odd:
-            best = (left_even, right_even)
-        else:
-            best = (left_odd, right_odd)
+        for i in range(len(s)):
+            for j in range(len(s) - 1, i -1, -1):
+                if j - i >= len(longest):
+                    is_palindrome = self.isPalindrome(s, i, j, palindromes)
+                    if is_palindrome:
+                        palindromes.add((i, j))
+                        if len(s[i:j+1]) > len(longest):
+                            longest = s[i:j+1]
+        return longest
 
-        return s[best[0]: best[1] + 1]
-
-    def max(self, best1: Tuple[int], best2: Tuple[int]):
-        if best1[1] - best1[0] > best2[1] - best2[0]:
-            return best1
-        return best2
-
-
-    def isPalindrome(self, s: str, left: int, right: int):
+    def isPalindrome(self, s, left, right, palindromes):
         """
         :param s:
         :param left:
         :param right:
+        :param palindromes:
         :return:
         """
-        if left >= right:
+        if (left, right) in palindromes or left >= right:
             return True
 
         if s[left] == s[right]:
-            return self.isPalindrome(s, left + 1, right - 1)
+            return self.isPalindrome(s, left + 1, right - 1, palindromes)
 
         return False
-
-    def longestPalindromeMiddleOutEven(self, s: str, mid: int):
-        left = mid - 1
-        right = mid
-
-        while left >= 0 and right < len(s) and s[left] == s[right]:
-            left -= 1
-            right += 1
-
-        return left + 1, right - 1
-
-    def longestPalindromeMiddleOutOdd(self, s: str, mid: int):
-        left = mid
-        right = mid
-
-        while left >= 0 and right < len(s) and s[left] == s[right]:
-            left -= 1
-            right += 1
-
-        return left + 1, right - 1
 
 
 if __name__ == '__main__':
     sol = Solution()
-    print(sol.longestPalindrome('bcabaaababc'))
+    print(sol.longestPalindrome('abadaba'))
