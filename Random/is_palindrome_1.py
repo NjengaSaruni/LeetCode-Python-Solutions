c8d1d94d2504178a568023a3aa8e6709e6092b32
Peter Njenga <peternjengask@gmail.com>
2019-06-21 20:35:26 +0300

    Longest palindromic substring working solution

--- Random/is_palindrome_1.py
+++ Random/is_palindrome_1.py
@@ -1,5 +1,4 @@
 def is_palindrome(string):
-
     left = 0
     right = len(string) - 1
 
@@ -9,5 +8,43 @@ def is_palindrome(string):
 
     return left >= right
 
+
+def longest_palindromic_substring(string):
+    longest = ''
+
+    MAX_LEFT = 0
+    MAX_RIGHT = len(string) - 1
+
+    for index, character in enumerate(string):
+
+        # This is the odd iteration
+        odd_longest = character
+        left = index - 1
+        right = index + 1
+        while left >= MAX_LEFT and right <= MAX_RIGHT and string[left] == string[right]:
+            odd_longest = string[left] + odd_longest + string[right]
+            left -= 1
+            right += 1
+
+        if len(longest) < len(odd_longest):
+            longest = odd_longest
+
+        # This is the even iteration
+        if index + 1 <= MAX_RIGHT and string[index + 1] == character:
+            even_longest = character + character
+            left = index - 1
+            right = index + 2
+
+            while left >= MAX_LEFT and right <= MAX_RIGHT and string[left] == string[right]:
+                even_longest = string[left] + even_longest + string[right]
+                left -= 1
+                right += 1
+
+            if len(longest) < len(even_longest):
+                longest = even_longest
+
+    return longest
+
+
 if __name__ == '__main__':
-    print(is_palindrome('aabbaa'))
+    print(longest_palindromic_substring('aabbaaa'))
