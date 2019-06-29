90f6accec12fbffaca7f09c81ef553ba76eee822
Peter Njenga <peternjengask@gmail.com>
2019-06-29 21:47:42 +0300

    Recursive power function with a bit of elegamce

--- QuickFire/Sorting/MergeSort/__init__.py
+++ QuickFire/Sorting/MergeSort/__init__.py
@@ -1,3 +1,18 @@
 """
 This module contains my quickfire solutions to the mergesort algorithm
 """
+
+def pow(x, y):
+    if y == 0:
+        return 1
+
+    power = x * pow(x, abs(y) - 1)
+
+    if y < 0 and power != 0:
+        power = 1 / power
+
+    return power
+
+
+if __name__ == '__main__':
+    print(pow(0.2, -2))
---
+++ Random/power_function.py
@@ -0,0 +1,4 @@
+"""
+This is a recursive approach to the power function.
+This was asked at a whiteboard interview at MicroSoft
+"""
