8d7cc43d9d85c79460812047df117d0f37229f66
Peter Njenga <peternjengask@gmail.com>
2019-07-04 10:09:11 +0300

    Undo nonsense

--- Andela/Sharon/coins.py
+++ Andela/Sharon/coins.py
@@ -1,26 +1,7 @@
 
 
 def change(value, coins):
-    current = set((coin,) for coin in coins)
-    done = set()
-
-    while len(current) > 0:
-        for set in current:
-            current_sum = sum(current[index])
-
-            if current_sum == value:
-                done.add(current[index])
-
-            else:
-                for coin in coins:
-                    if coin + current_sum <= value:
-                        original = list(current[index])
-                        original.append(coin)
-                        current.add(set(original))
-
-            del current[index]
-    return done
-
+=
 
 if __name__ == '__main__':
-    print(change(10, [1, 2, 3]))
+    print(change(4, [1, 2]))
